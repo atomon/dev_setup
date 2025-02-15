663f8f3643d325762f65e68fd03c77df7918098c
OHNISHI <38009289+atomon@users.noreply.github.com>
2025-02-15 18:47:56 +0900

    feat: add ctrl2cap tools

---
+++ windows/capslock2ctrl.ps1
@@ -0,0 +1,23 @@
+<#=====================
+Download ctrl2cap tools
+=====================#>
+cd $home/Downloads
+curl -O https://download.sysinternals.com/files/Ctrl2Cap.zip
+7z e -o* .\Ctrl2Cap.zip
+
+<#===========
+Administrator
+===========#>
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process wt.exe "pwsh -wd $path `"$PSCommandPath`"" -Verb RunAs; exit }
+
+<#==============
+Install ctrl2cap
+==============#>
+cd .\Ctrl2Cap
+.\ctrl2cap.exe /install
+
+echo @"
+
+Successful installation! 😎 🎉 
+Reboot is required!!
+"@
